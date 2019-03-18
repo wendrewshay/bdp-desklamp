ab288d8e8155382affa7192324f495a4a497baa2
Joney <joney@ourqq.com>
2019-03-18 14:33:39 +0800

    新建网关项目

---
+++ bdp-gateway/src/main/java/com/desklamp/gateway/BdpGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.desklamp.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdpGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BdpGatewayApplication.class, args);
+    }
+
+}
---
+++ bdp-gateway/src/test/java/com/desklamp/gateway/BdpGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.desklamp.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BdpGatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
