3fade20b4fdc5b4468c774983d4189cb78c4e53d
Joney <joney@ourqq.com>
2019-03-21 20:58:57 +0800

    完善配置文件信息

--- bdp-services/bdp-enterprise-data/src/main/java/com/desklamp/enterprise/data/controller/CompanyController.java
+++ bdp-services/bdp-enterprise-data/src/main/java/com/desklamp/enterprise/data/controller/CompanyController.java
@@ -4,6 +4,8 @@ import com.desklamp.enterprise.data.domain.CompanyInfo;
 import com.desklamp.enterprise.data.service.CompanyService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,4 +24,5 @@ public class CompanyController {
         CompanyInfo companyInfo = this.companyService.queryById(id);
         return companyInfo;
     }
+
 }
